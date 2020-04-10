5ee9d0ec625031c9e85ac94d7a8c1daeaf7598bb
neitro <admin@rnd2.ru>
2020-04-11 00:25:28 +0300

    Added tests for nested case

--- tests/test_xmltodict.py
+++ tests/test_xmltodict.py
@@ -18,3 +18,18 @@ def test_simple_nested_case():
     result = XmlToDict(etree_element).get_dict()
     assert result == expected_result, result
 
+
+def test_simple_multi_different_nested_case():
+    text = "<root><node1>1</node1><node2>2</node2></root>"
+    etree_element = ElementTree.fromstring(text)
+    expected_result = {'root': {'node1': '1', 'node2': '2'}}
+    result = XmlToDict(etree_element).get_dict()
+    assert result == expected_result, result
+
+
+def test_simple_multi_same_nested_case():
+    text = "<root><node>1</node><node>2</node></root>"
+    etree_element = ElementTree.fromstring(text)
+    expected_result = {'root': {'node': ['1', '2']}}
+    result = XmlToDict(etree_element).get_dict()
+    assert result == expected_result, result
