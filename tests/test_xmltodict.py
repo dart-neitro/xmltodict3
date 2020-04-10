182bf86362f3ea2ee52727c2df739a5b41ca4575
neitro <admin@rnd2.ru>
2020-04-10 23:26:18 +0300

    Updated tests

--- tests/test_xmltodict.py
+++ tests/test_xmltodict.py
@@ -1,9 +1,11 @@
 import xml.etree.ElementTree as ElementTree
 
+from xmltodict3 import XmlToDict
+
 
 def test_simple_case():
     text = "<root>1</root>"
     etree_element = ElementTree.fromstring(text)
     expected_result = {'root': '1'}
-    result = None
+    result = XmlToDict(etree_element).get_dict()
     assert result == expected_result, result
