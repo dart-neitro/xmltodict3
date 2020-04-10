a4cd8b260274fe89aa979d14ad7147a8bc7e6c2e
neitro <admin@rnd2.ru>
2020-04-11 00:33:47 +0300

    Added implementation for nested cases

--- xmltodict3/__init__.py
+++ xmltodict3/__init__.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import xml.etree.ElementTree as ElementTree
 
 
@@ -14,12 +15,24 @@ class XmlToDict:
             value = self.node.text
         return {tag: value}
 
-    def get_children_data(self):
-        node_data = {}
-        for child_node in self.children_nodes:
-            tag = child_node.get_tag()
-            node_data[tag] = child_node.get_dict()[tag]
-        return node_data
-
     def get_tag(self):
         return self.node.tag
+
+    def get_children_data(self):
+        node_data = defaultdict(list)
+        for child_node in self.children_nodes:
+            tag = child_node.get_tag()
+            node_data[tag].append(child_node.get_dict()[tag])
+        node_data = self.group_children_data(node_data)
+        return node_data
+
+    def group_children_data(self, children_data):
+        grouped_data = dict()
+        for tag in children_data:
+            sub_node_data = children_data[tag]
+            if len(sub_node_data) == 1:
+                grouped_data[tag] = sub_node_data[0]
+            else:
+                grouped_data[tag] = sub_node_data
+        return grouped_data
+
