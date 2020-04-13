414e2872279067ad207bb795cc566f2089437698
neitro <admin@rnd2.ru>
2020-04-13 23:12:14 +0300

    Added the test for working with file

---
+++ tests/test_xml_file_to_dict.py
@@ -0,0 +1,54 @@
+import os
+
+from xmltodict3 import XmlFileToDict
+
+
+def test_simple_case():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sample_xml_file_path = os.path.join(
+        current_path, 'test_data/sample_food.xml')
+    result = XmlFileToDict(sample_xml_file_path).get_dict()
+    expected_result = {
+        'breakfast_menu': {
+            'food': [{
+                'calories': '650',
+                'description': 'Two of our famous Belgian '
+                'Waffles with plenty of real '
+                'maple syrup',
+                'name': 'Belgian Waffles',
+                'price': '$5.95'
+            },
+            {
+                'calories': '900',
+                'description': 'Light Belgian waffles covered '
+                'with strawberries and whipped '
+                'cream',
+                'name': 'Strawberry Belgian Waffles',
+                'price': '$7.95'
+            },
+            {
+                'calories': '900',
+                'description': 'Light Belgian waffles covered '
+                'with an assortment of fresh '
+                'berries and whipped cream',
+                'name': 'Berry-Berry Belgian Waffles',
+                'price': '$8.95'
+            },
+            {
+                'calories': '600',
+                'description': 'Thick slices made from our '
+                'homemade sourdough bread',
+                'name': 'French Toast',
+                'price': '$4.50'
+            },
+            {
+                'calories': '950',
+                'description': 'Two eggs, bacon or sausage, '
+                'toast, and our ever-popular hash '
+                'browns',
+                'name': 'Homestyle Breakfast',
+                'price': '$6.95'
+            }]
+        }
+    }
+    assert result == expected_result, result
