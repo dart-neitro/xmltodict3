e1ac63d1ee3adc897f648b01eb8e00a9ed6927c3
neitro <admin@rnd2.ru>
2020-04-23 00:03:56 +0300

    Added TransformerException to scope

--- xmltodict3/__init__.py
+++ xmltodict3/__init__.py
@@ -2,3 +2,4 @@ from xmltodict3.xml_to_dict import XmlToDict, XmlFileToDict, XmlTextToDict
 from xmltodict3.transformers import (
     IntegerTransformer, BoolTransformer, DateTimeTransformer,
     PullTransformers, DefaultTransformerList)
+from xmltodict3.exceptions import TransformerException
