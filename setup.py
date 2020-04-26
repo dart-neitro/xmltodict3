fd07fc8faf376bc25018885ca819792e0230a3a2
neitro <admin@rnd2.ru>
2020-04-26 23:09:04 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='xmltodict3',
+    version='0.0.1',
+    description='An open-source library that is used '
+                'for converting XML to a python dictionary.',
+    url='https://github.com/dart-neitro/xmltodict3',
+    author='Konstantin Neitro',
+    author_email='neitro88@gmail.com',
+    packages=find_packages(include=['xmltodict3']),
+)
