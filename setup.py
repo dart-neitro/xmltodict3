471ca4ea55d4e70ab2492e23256aa293dcfcc591
neitro <admin@rnd2.ru>
2020-05-14 00:24:39 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='xmltodict3',
-    version='0.0.3',
+    version='0.0.4',
     description='An open-source library that is used '
                 'for converting XML to a python dictionary.',
     long_description=long_description,
