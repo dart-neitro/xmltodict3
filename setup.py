d45b9b9d4dc9b1c92776db29eda9a5ccc39ab772
neitro <admin@rnd2.ru>
2020-05-13 23:21:34 +0300

    Updated CI

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='xmltodict3',
-    version='0.0.1',
+    version='0.0.2',
     description='An open-source library that is used '
                 'for converting XML to a python dictionary.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/dart-neitro/xmltodict3',
     author='Konstantin Neitro',
     author_email='neitro88@gmail.com',
